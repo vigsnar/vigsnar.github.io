0e82fd4e5c764bb5eee876227bb9d9375fa35825
Sebastian Gaviria <professionellys@gmail.com>
2025-03-28 19:14:00 -0400

    Adjust canvas size

--- js/rgba.js
+++ js/rgba.js
@@ -111,8 +111,8 @@ void main(void) {
         document.body.append(canvas);
         if (false === config.fullscreen)
             return
-        addEventListener("resize", () => this.resize(window.innerWidth, window.innerHeight));
-        this.newSize(window.innerWidth, window.innerHeight);
+        addEventListener("resize", () => this.resize(document.innerWidth, document.innerHeight));
+        this.newSize(document.innerWidth, document.innerHeight);
     } else {
         this.newSize(canvas.width, canvas.height);
     }
